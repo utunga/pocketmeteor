125c3c186a7033ea38c35e6f69652b5b3cfde7e0
utunga <utunga@gmail.com>
2013-04-07 13:44:40 +1200

    Update package.js

--- packages/mongowrapper/package.js
+++ packages/mongowrapper/package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: "Attempt to add mongo support to minimongo collection"
+  summary: "Attempt to add Aggregate support to minimongo collection"
 });
 
 Npm.depends({mongodb: "1.2.14"});
